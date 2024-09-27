6de22e284738112f43d9510e54bdb89b10598166
Sotatek-SonNguyen16 <son.nguyen16@sotatek.com>
2024-09-27 15:42:07 +0700

    initital server

---
+++ server/src/main/java/sonnh/opt/opt_plan/OptPlanApplication.java
@@ -0,0 +1,13 @@
+package sonnh.opt.opt_plan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptPlanApplication.class, args);
+	}
+
+}
