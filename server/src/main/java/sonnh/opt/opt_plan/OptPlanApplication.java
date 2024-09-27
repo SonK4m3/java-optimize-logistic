e8f39a54cf0f888a49b0fac1fdb116536b548d3e
Sotatek-SonNguyen16 <son.nguyen16@sotatek.com>
2024-09-27 17:17:32 +0700

    feat: runing server

--- server/src/main/java/sonnh/opt/opt_plan/OptPlanApplication.java
+++ server/src/main/java/sonnh/opt/opt_plan/OptPlanApplication.java
@@ -2,8 +2,10 @@ package sonnh.opt.opt_plan;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"main.java.sonnh.opt.opt_plan.controller", "main.java.sonnh.opt.opt_plan.service"})
 public class OptPlanApplication {
 
 	public static void main(String[] args) {
---
+++ server/src/main/java/sonnh/opt/opt_plan/controller/AuthController.java
@@ -0,0 +1,19 @@
+package main.java.sonnh.opt.opt_plan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @GetMapping("/login")
+    public String login() {
+        return "Login endpoint";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "Registration endpoint";
+    }
+}
